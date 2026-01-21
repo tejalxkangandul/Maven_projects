6655dceebfedd2cf443f0955d8f0676e8e73a97b
Tejal <tejalkangandul9@gmail.com>
2026-01-21 09:48:27 +0530

    Add files via upload

---
+++ EmployeeMavenJdbc_SU2/src/main/java/com/example/DBUtil.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/skill_db",
+                "root",
+                "tejal1"
+        );
+    }
+}
