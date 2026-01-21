d24489d0ba2cab07f4b63f510235d94b4a1f3ffa
Tejal <tejalkangandul9@gmail.com>
2026-01-21 09:47:09 +0530

    Add files via upload

---
+++ BtwPrimeNumber/src/main/java/com/su2/App.java
@@ -0,0 +1,43 @@
+package com.su2;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter start number: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end number: ");
+        int end = sc.nextInt();
+
+        System.out.println("Prime numbers between " + start + " and " + end + " are:");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        sc.close();
+    }
+}
